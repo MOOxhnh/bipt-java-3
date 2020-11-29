729cfa33cd81eacca6788d0b078cb71c43583c5d
MOOxhnh <72553762+MOOxhnh@users.noreply.github.com>
2020-11-29 21:26:50 +0800

    Add files via upload

---
+++ StudentManagement.java
@@ -0,0 +1,8 @@
+    package interfaceApplication;
+
+public interface StudentManagement {//作为学生身份的“行为标准”
+	int buzhu = 800;    //补贴
+	void payTuition(double tuition);//缴纳学费
+	void searchTuition(double tuition);//查询学费
+
+}
